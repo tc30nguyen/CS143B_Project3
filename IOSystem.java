f2f302c4159d042ddf5b59d9504565f946c498ab
TC <tc30nguyen@gmail.com>
2014-06-03 00:24:59 -0700

    added save method

--- IOSystem.java
+++ IOSystem.java
@@ -1,21 +1,59 @@
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class IOSystem 
 {
 	private char[][] ldisk;
+	private final int numOfBlocks;
+	private final int blockLength;
 
 	IOSystem(int numOfBlocks, int blockLength)
 	{
+		this.numOfBlocks = numOfBlocks;
+		this.blockLength = blockLength;
 		ldisk = new char[numOfBlocks][blockLength];
 	}
 
-	void read_block(int index, char[] readTo)
+	public void read_block(int index, char[] readTo)
 	{
 		char[] toRead = ldisk[index];
 		readTo = Arrays.copyOf(toRead, toRead.length);
 	}
 
-	void write_block(int index, char[] readFrom)
+	public void write_block(int index, char[] readFrom)
 	{
 		char[] toWrite = ldisk[index];
 		toWrite = Arrays.copyOf(readFrom, toWrite.length);
+	}
+
+	public void save(String fileName)
+	{
+		try
+		{
+		File output = new File(fileName);
+		if(!output.exists())
+			output.createNewFile();
+
+		StringBuilder sb = new StringBuilder();
+		FileWriter fw = new FileWriter(output.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		for(int i = 0; i < numOfBlocks; i++)
+		{
+			for(int j = 0; j < blockLength; j++)
+				sb.append(ldisk[i][j]);
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
