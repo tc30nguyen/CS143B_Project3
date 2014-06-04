0764fb90a21d0952bae77eeb19ab6bdc7b3bbe09
TC <tc30nguyen@gmail.com>
2014-06-04 04:34:08 -0700

    added feature to run from an input file

--- Driver.java
+++ Driver.java
@@ -3,7 +3,12 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		Shell shell = new Shell();
+		Shell shell;
+		if(args.length == 0)
+			shell = new Shell();
+		else
+			shell = new Shell(args[0]);
+		
 		shell.run();
 	}
 }
--- Shell.java
+++ Shell.java
@@ -1,85 +1,153 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Shell 
 {
+	BufferedReader inputFileReader;
 	FileSystem fileSystem;
 	Scanner input;
 
 	public Shell()
 	{
+		inputFileReader = null;
 		input = new Scanner(System.in);
+	}
+
+	public Shell(String inputFileName)
+	{
+		try 
+		{
+			inputFileReader = new BufferedReader(new FileReader(inputFileName));
+			input = null;
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public void run()
 	{
 		String currentLine = "";
-		
-		while(input.hasNextLine() && !currentLine.equals("exit"))
-		{
-			currentLine = input.nextLine();
-			Scanner lineScanner = new Scanner(currentLine);
-			String command;
 
-			if(lineScanner.hasNext())
+		//run through System.input
+		if(inputFileReader == null)
+		{	
+			while(input.hasNextLine() && !currentLine.equals("exit"))
 			{
-				command = lineScanner.next();
-
-				switch(command)
+				currentLine = input.nextLine();
+				runCommand(currentLine);
+			}
+			
+			input.close();
+		}
+		
+		//run through an input file
+		else
+		{
+			try 
+			{
+				currentLine = inputFileReader.readLine();
+				
+				while(currentLine != null)
 				{
-					case "cr":
-						String name;
-						if(lineScanner.hasNext())
-						{
-							name = lineScanner.next();
-							fileSystem.create(name.toCharArray());
+					if(!currentLine.isEmpty())
+						runCommand(currentLine);
+					currentLine = inputFileReader.readLine();
+				}
+			}
+			
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void runCommand(String currentLine)
+	{
+		Scanner lineScanner = new Scanner(currentLine);
+		String command;
+		if(lineScanner.hasNext())
+		{
+			command = lineScanner.next();
+			switch(command)
+			{
+				//create: cr <name>
+				case "cr":
+					String name;
+					if(lineScanner.hasNext())
+					{
+						name = lineScanner.next();
+						if(fileSystem.create(name.toCharArray()))
 							System.out.println(name + " created");
-						}
 						else
 							System.out.println("error");
-						break;
+					}
+					else
+						System.out.println("error");
+					break;
 
-					case "de":
-						System.out.println("NOT DONE");
-						break;
+				//delete: de <name>
+				case "de":
+					if(lineScanner.hasNext())
+					{
+						name = lineScanner.next();
+						if(fileSystem.destroy(name.toCharArray()))
+							System.out.println(name + " destroyed");
+						else
+							System.out.println("error");
+					}
+					else
+						System.out.println("error");
+					break;
 
-					case "op":
-						//String name;
-						int OFTIndex = -1;
-						if(lineScanner.hasNext())
-						{
-							name = lineScanner.next();
-							OFTIndex = fileSystem.open(name.toCharArray());
+				//open: op <name>
+				case "op":
+					int OFTIndex = -1;
+					if(lineScanner.hasNext())
+					{
+						name = lineScanner.next();
+						OFTIndex = fileSystem.open(name.toCharArray());
+						if(OFTIndex != -1)
 							System.out.println(name + " opened " + OFTIndex);
-						}
 						else
 							System.out.println("error");
-						break;
+					}
+					else
+						System.out.println("error");
+					break;
 
-					case "cl":
+				//close: cl <OFT index>
+				case "cl":
+					if(lineScanner.hasNextInt())
+					{
+						OFTIndex = lineScanner.nextInt();
+						fileSystem.close(OFTIndex);
+						System.out.println("file " + OFTIndex + " closed");
+					}
+					else
+						System.out.println("error");
+					break;
+
+				//read: rd <OFT index> <bytesToRead>
+				case "rd":
+					char[] buffer;
+					int bytesToRead = 0;
+					if(lineScanner.hasNextInt())
+					{
+						OFTIndex = lineScanner.nextInt();
 						if(lineScanner.hasNextInt())
 						{
-							OFTIndex = lineScanner.nextInt();
-							fileSystem.close(OFTIndex);
-							System.out.println("file " + OFTIndex + " closed");
-						}
-						else
-							System.out.println("error");
-						break;
-
-					case "rd":
-						char[] buffer;
-						int bytesToRead;
-						//int OFTIndex = -1;
-						if(lineScanner.hasNextInt())
-						{
-							OFTIndex = lineScanner.nextInt();
-							if(lineScanner.hasNextInt())
+							bytesToRead = lineScanner.nextInt();
+							buffer = new char[bytesToRead];
+							bytesToRead = fileSystem.read(OFTIndex, buffer, bytesToRead);
+							if(bytesToRead != -1)
 							{
-								bytesToRead = lineScanner.nextInt();
-								buffer = new char[bytesToRead];
-								bytesToRead = fileSystem.read(OFTIndex, buffer, bytesToRead);
-
 								//print output
 								for(int i = 0; i < bytesToRead; i++)
 									System.out.print(buffer[i]);
@@ -90,94 +158,95 @@ public class Shell
 						}
 						else
 							System.out.println("error");
-						break;
-
-					case "wr":
-						//char[] buffer;
-						char toWrite = 0;
-						int count = 0;
-						//int OFTIndex = -1;
-						if(lineScanner.hasNextInt())
-						{
-							OFTIndex = lineScanner.nextInt();
-							if(lineScanner.hasNext())
-								toWrite = lineScanner.next().charAt(0);
-							else
-								System.out.println("error");
-							if(lineScanner.hasNextInt())
-								count = lineScanner.nextInt();
-							else
-								System.out.println("error");
-
-							buffer = new char[count];
-							Arrays.fill(buffer, toWrite);
-							count = fileSystem.write(OFTIndex, buffer, count);
-							
-							if(count == -1)
-								System.out.println("error");
-							else
-								System.out.println(count + " bytes written");
-						}
-						else
-							System.out.println("error");
-						break;
-
-					case "sk":
-						//int OFTIndex = -1;
-						int filePosition = 0;
-						if(lineScanner.hasNextInt())
-						{
-							OFTIndex = lineScanner.nextInt();
-							if(lineScanner.hasNextInt())
-							{
-								filePosition = lineScanner.nextInt();
-								fileSystem.lseek(OFTIndex, filePosition);
-								System.out.println("current position is " + filePosition);
-							}
-							else
-								System.out.println("error");
-						}
-						else
-							System.out.println("error");
-						break;
-
-					case "in":
-						String fileName;
-						if(lineScanner.hasNext())
-						{;
-							fileSystem = new FileSystem(lineScanner.next());
-							System.out.println("disk restored");
-						}
-						else
-						{
-							fileSystem = new FileSystem();
-							System.out.println("disk initialized");
-						}
-						break;
-
-					case "sv":
-						//String fileName;
-						if(lineScanner.hasNext())
-						{
-							fileName = lineScanner.next();
-							fileSystem.save(fileName);
-							System.out.println("disk saved");
-						}
-						break;
-						
-					case "dr":
-						fileSystem.directory();
-						break;
-
-					default:
+					}
+					else
 						System.out.println("error");
-						break;
-				}
+					break;
+
+				//write: wr <OFT index> <charToWrite> <bytesToWrite>
+				case "wr":
+					char toWrite = 0;
+					int count = 0;
+					if(lineScanner.hasNextInt())
+					{
+						OFTIndex = lineScanner.nextInt();
+						if(lineScanner.hasNext())
+							toWrite = lineScanner.next().charAt(0);
+						else
+							System.out.println("error");
+						if(lineScanner.hasNextInt())
+							count = lineScanner.nextInt();
+						else
+							System.out.println("error");
+						buffer = new char[count];
+						Arrays.fill(buffer, toWrite);
+						count = fileSystem.write(OFTIndex, buffer, count);
+						
+						if(count == -1)
+							System.out.println("error");
+						else
+							System.out.println(count + " bytes written");
+					}
+					else
+						System.out.println("error");
+					break;
+
+				//seek: sk <OFT index> <position>
+				case "sk":
+					int filePosition = 0;
+					if(lineScanner.hasNextInt())
+					{
+						OFTIndex = lineScanner.nextInt();
+						if(lineScanner.hasNextInt())
+						{
+							filePosition = lineScanner.nextInt();
+							fileSystem.lseek(OFTIndex, filePosition);
+							System.out.println("current position is " + filePosition);
+						}
+						else
+							System.out.println("error");
+					}
+					else
+						System.out.println("error");
+					break;
+
+				//initialize: in OR in <inputFileName.txt>
+				case "in":
+					String fileName;
+					if(lineScanner.hasNext())
+					{;
+						fileSystem = new FileSystem(lineScanner.next());
+						System.out.println("disk restored");
+					}
+					else
+					{
+						fileSystem = new FileSystem();
+						System.out.println("disk initialized");
+					}
+					break;
+
+				//save: sv <outputFileName.txt>
+				case "sv":
+					if(lineScanner.hasNext())
+					{
+						fileName = lineScanner.next();
+						fileSystem.save(fileName);
+						System.out.println("disk saved");
+					}
+					break;
+				
+				//directory: dr
+				case "dr":
+					fileSystem.directory();
+					break;
+
+				//unhandled input
+				default:
+					System.out.println("error");
+					break;
 			}
-			
-			lineScanner.close();
 		}
 		
-		input.close();
+		lineScanner.close();
 	}
 }
